723a08ef2b232f213fbf6078f682f42b04642d23
Fiona <fionat119@icloud.com>
2025-08-20 11:00:44 -0600

    prettier fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div className='flex flex-row items-center gap-2 md:flex-col'>
+    <div className="flex flex-row items-center gap-2 md:flex-col">
       <h1>hello</h1>
       <h1>world</h1>
     </div>
