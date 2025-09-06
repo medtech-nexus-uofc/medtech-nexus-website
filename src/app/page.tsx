baa3f3ee40787f877890561e8d1a648cd1c72bb7
Fiona <fionat119@icloud.com>
2025-09-06 14:17:49 -0600

    added font

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
-import Image from 'next/image'
+import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="flex flex-row items-center gap-2 md:flex-col">
-      <h1>hello</h1>
-      <h1>world</h1>
+      <h1 className="font-assistant text-3xl">abcdefghijklmnopqrstuvwxyz</h1>
+      <h1 className="font-funnel-display text-3xl">
+        abcdefghijklmnopqrstuvwxyz
+      </h1>
+      <p className="text-3xl">abcdefghijklmnopqrstuvwxyz</p>
     </div>
-  )
+  );
 }
