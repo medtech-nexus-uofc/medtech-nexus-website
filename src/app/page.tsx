d0bd679940c971c720ff7961385c34aa57d8c236
Fiona <fionat119@icloud.com>
2025-09-06 14:22:56 -0600

    fixed fonts not displaying

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,10 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <div className="flex flex-row items-center gap-2 md:flex-col">
-      <h1 className="font-assistant text-3xl">abcdefghijklmnopqrstuvwxyz</h1>
+      <h1 className="text-3xl">abcdefghijklmnopqrstuvwxyz</h1>
       <h1 className="font-funnel-display text-3xl">
         abcdefghijklmnopqrstuvwxyz
       </h1>
-      <p className="text-3xl">abcdefghijklmnopqrstuvwxyz</p>
     </div>
   );
 }
